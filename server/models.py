808158a7ea166bba58b0e8f2195700f061275374
Ariel Gerardo Rios <arielgerardorios@gmail.com>
2011-06-20 02:46:09 -0300

    FIX: indentation + pep8 safe.

--- server/models.py
+++ server/models.py
@@ -32,47 +32,47 @@ class Server(modes.Model):
     def __unicode__(self):
         return u"%s [%s:%d]" % (self.name, self.ip, self.port)
 
-   def connect(self):                                                               
-       """
-       Stablish a connection using current parameters.
-       """
-       params = {"host": self.ip, "port": self.port, "user": self.username, \
-               "passwd": self.password}
-       logger.info("Connecting to MySQL server with params '%s'." % \
-               repr(params))
-       self.conn = MySQLdb.connect(**params)
-                                                                            
-   def close(self):
-       """
-       Close the current connection.
-       """
-       try:
-           self.conn.close()
-       except:
-           pass
-       logger.debug("Connection closed.")
-                                                                            
-   def doquery(self, sql, parsefunc=None):
-       """
-       """
-       cur = self.conn.cursor()
-       logger.debug("Executing query: %s" % sql)
-       cur.execute(sql)
-       result = cur.fetchall()
-       logger.debug("Result: %s" % repr(result))
-       return parsefunc(result) if parsefunc else result
-                                                                            
-   def changedb(self, dbname):
-       """
-       """
-       self.doquery("USE %s;" % dbname)
-                                                                            
-   def show_variables(self):
-       """
-       """
-       return self.doquery("SHOW VARIABLES;", dict)
-                                                                            
-   def show_status(self):
-       """
-       """
-       return self.doquery("SHOW STATUS;", dict)
+    def connect(self):
+        """
+        Stablish a connection using current parameters.
+        """
+        params = {"host": self.ip, "port": self.port, "user": self.username, \
+                "passwd": self.password}
+        logger.info("Connecting to MySQL server with params '%s'." % \
+                repr(params))
+        self.conn = MySQLdb.connect(**params)
+
+    def close(self):
+        """
+        Close the current connection.
+        """
+        try:
+            self.conn.close()
+        except:
+            pass
+        logger.debug("Connection closed.")
+
+    def doquery(self, sql, parsefunc=None):
+        """
+        """
+        cur = self.conn.cursor()
+        logger.debug("Executing query: %s" % sql)
+        cur.execute(sql)
+        result = cur.fetchall()
+        logger.debug("Result: %s" % repr(result))
+        return parsefunc(result) if parsefunc else result
+
+    def changedb(self, dbname):
+        """
+        """
+        self.doquery("USE %s;" % dbname)
+
+    def show_variables(self):
+        """
+        """
+        return self.doquery("SHOW VARIABLES;", dict)
+
+    def show_status(self):
+        """
+        """
+        return self.doquery("SHOW STATUS;", dict)
