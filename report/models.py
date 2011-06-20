959896c0c9e2653e303a4ca98fedc53cbd6e0d33
Ariel Gerardo Rios <arielgerardorios@gmail.com>
2011-06-20 02:27:22 -0300

    ADD: basic models for report app.

--- report/models.py
+++ report/models.py
@@ -1,3 +1,39 @@
+# models
 from django.db import models
+from server.models import Server
 
-# Create your models here.
+# utils
+from django.utils.translation import ugettext as _
+import settings
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class Report(models.Model):
+    """
+    """
+    server = models.ForeignKey(Server)
+    title = models.CharField(_("Report title"), help_text="Title for this \
+            report.")
+
+    def __unicode__(self):
+        """
+        """
+        return u"Report '%s'" % (self.title)
+
+
+class Section(models.Model):
+    """
+    """
+    report = models.ForeignKey(Report)
+    title = models.CharField(_("Part title"), help_text="Title for this \
+            section of the report.")
+    variables = models.CharField(_("Variables"), help_text="Variables involved \
+            in this section.")
+
+    def __unicode__(self):
+        """
+        """
+        return u"Section '%s' for %s" % (self.title, self.report)
