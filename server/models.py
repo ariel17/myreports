f0772a97a82da7bdde43c0bd832651971799e999
Ariel Gerardo Rios <arielgerardorios@gmail.com>
2011-06-20 02:07:10 -0300

    FIX: merged classes.

--- server/models.py
+++ server/models.py
@@ -13,58 +13,6 @@ import MySQLdb
 logger = logging.getLogger(__name__)
 
 
-class MySQLHandler(object):
-    """
-    """
-    def __init__(self, **kwargs):
-        super(MySQLHandler, self).__init__()
-        self.username = kwargs["username"]
-        self.password = kwargs["password"]
-        self.host = kwargs["host"]
-        self.port = kwargs.get("port", 3306)
-        self.db = kwargs.get("db", None)
-
-    def connect(self):
-        """
-        Stablish a connection using current parameters.
-        """
-        params = {"host": self.host, "port": self.port, "user": self.user, \
-                "passwd": self.passwd}
-        if self.db:
-            params["db"] = self.db
-        logger.info("Connecting to MySQL server with params '%s'." % \
-                repr(params))
-        self.conn = MySQLdb.connect(**params)
-
-    def close(self):
-        """
-        Close the current connection.
-        """
-        try:
-            self.conn.close()
-        except:
-            pass
-        logger.debug("Connection closed.")
-
-    def doquery(self, sql, parsefunc=None):
-        """
-        """
-        cur = self.conn.cursor()
-        logger.debug("Executing query: %s" % sql)
-        cur.execute(sql)
-        result = cur.fetchall()
-        logger.debug("Result: %s" % repr(result))
-        return parsefunc(result) if parsefunc else result
-
-    def changedb(self, dbname):
-        """
-        """
-        self.doquery("USE %s;" % dbname)
-
-    class Meta:
-        abstract = True
-
-
 class Server(modes.Model):
     """
     MySQL Server instance wich will be used to generate reports.
@@ -83,3 +31,48 @@ class Server(modes.Model):
 
     def __unicode__(self):
         return u"%s [%s:%d]" % (self.name, self.ip, self.port)
+
+   def connect(self):                                                               
+       """
+       Stablish a connection using current parameters.
+       """
+       params = {"host": self.ip, "port": self.port, "user": self.username, \
+               "passwd": self.password}
+       logger.info("Connecting to MySQL server with params '%s'." % \
+               repr(params))
+       self.conn = MySQLdb.connect(**params)
+                                                                            
+   def close(self):
+       """
+       Close the current connection.
+       """
+       try:
+           self.conn.close()
+       except:
+           pass
+       logger.debug("Connection closed.")
+                                                                            
+   def doquery(self, sql, parsefunc=None):
+       """
+       """
+       cur = self.conn.cursor()
+       logger.debug("Executing query: %s" % sql)
+       cur.execute(sql)
+       result = cur.fetchall()
+       logger.debug("Result: %s" % repr(result))
+       return parsefunc(result) if parsefunc else result
+                                                                            
+   def changedb(self, dbname):
+       """
+       """
+       self.doquery("USE %s;" % dbname)
+                                                                            
+   def show_variables(self):
+       """
+       """
+       return self.doquery("SHOW VARIABLES;", dict)
+                                                                            
+   def show_status(self):
+       """
+       """
+       return self.doquery("SHOW STATUS;", dict)
