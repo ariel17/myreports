d5f1ebf344e18b45bb94b9645a632e64711207b2
Ariel Gerardo Rios <arielgerardorios@gmail.com>
2011-08-22 23:00:02 -0300

    FIX: Only add Snapshot model into admin console if settings.DEBUG is True.

--- history/admin.py
+++ history/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
+from django.conf import settings
 
-from models import Snapshot
-
-
-class SnapshotAdmin(admin.ModelAdmin):
-    list_display = ("server", "variable", "time", "value" )
-    list_filter = ("server", "variable", "time")
-    ordering = ("server", "variable", )
-
-
-admin.site.register(Snapshot, SnapshotAdmin)
+if settings.DEBUG:
+    from models import Snapshot
+    
+    class SnapshotAdmin(admin.ModelAdmin):
+        list_display = ("server", "variable", "time", "value" )
+        list_filter = ("server", "variable", "time")
+        ordering = ("server", "variable", )
+    
+    admin.site.register(Snapshot, SnapshotAdmin)
