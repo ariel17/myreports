ca58174140d092125c1c211905cd44bd36a9f681
Ariel Gerardo Rios <arielgerardorios@gmail.com>
2011-09-04 16:14:48 -0300

    ADD: get object or 404 response for 'report' views.

--- report/views.py
+++ report/views.py
@@ -2,17 +2,21 @@ from django.http import (HttpResponse, HttpResponseRedirect,
         HttpResponseBadRequest, HttpRequest)
 from django.shortcuts import render_to_response, Http404, get_object_or_404
 
-from report.models import Section
+from report.models import Report, Section
+from server.models import Server
 
 
-def show_report(request, id):
+def show_report(request, report_id, server_id):
     """
     """
-    pass
+    report = get_object_or_404(Report, id=report_id)
+    server = get_obejct_or_404(Server, id=server_id)
+    render_to_response('report.html')
 
 
-def show_section(request, id):
+def show_section(request, section_id, server_id):
     """
     """
-    section = get_object_or_404(Section, id=id)
+    section = get_object_or_404(Section, id=section_id)
+    server = get_obejct_or_404(Server, id=server_id)
     return render_to_response('section.html')
