f2266d46ff78e5b45c00ebb3bfa589a165c0965c
Ariel Gerardo Rios <arielgerardorios@gmail.com>
2011-09-04 13:54:31 -0300

    CHG: import order for HTTP objects in server/views.py

--- server/views.py
+++ server/views.py
@@ -1,5 +1,5 @@
-from django.http import (HttpResponse, HttpResponseRedirect,
-        HttpResponseBadRequest, HttpRequest)
+from django.http import (HttpRequest, HttpResponse, HttpResponseRedirect,
+        HttpResponseBadRequest)
 from django.shortcuts import render_to_response, Http404, get_object_or_404
 
 from server.models import Server
