fc989249bd75a99092a65c48c5f47faf9e613bcb
Ariel Gerardo Rios <arielgerardorios@gmail.com>
2011-09-05 08:36:15 -0300

    ADD: direct link to a single report's section in reports.

--- report/views.py
+++ report/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 
 from server.models import ReportByServer
 from history.models import Snapshot
+from report.models import Section
 
 
 import logging
@@ -40,4 +41,26 @@ def show_report(request, uuid=None):
 def show_section(request, uuid, id):
     """
     """
-    pass
+
+    s = get_object_or_404(Section, id=id)
+    rs = get_object_or_404(ReportByServer, uuid=uuid)
+    params = {'section': s, 'sections': {},}
+                                                                               
+    if not s.period:
+        if rs.server.connect():
+            ss = Snapshot.get_current_values(rs.server, s.variables.all())
+            
+            logger.debug("Current values collected for section: %s" %
+                    repr(ss))
+            params['sections'][s.id] = ss
+        else:
+            # TODO: return 500
+            pass
+                                                                        
+    else:
+        ss = Snapshot.get_history(rs.server, s.variables.all())
+        logger.debug("History collected for section: %s" % repr(ss))
+        params['sections'][s.id] = ss
+                                                                               
+    logger.debug("Params to use: %s" % repr(params))
+    return render_to_response('section.html', params)
