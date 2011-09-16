eb97b79843e7d6111ed4eb042867f53312bbb4ee
Ariel Gerardo Rios <arielgerardorios@gmail.com>
2011-09-15 21:32:41 -0300

    ADD: Factory pattern implemented to Snapshot model to diferenciate between single variables and usage. This is for #9.

--- history/models.py
+++ history/models.py
@@ -1,32 +1,35 @@
-import datetime
+# This model file implements the factory pattern to determine with Snapshot
+# class must be instantiated. This particular approach is based in
+# this StackOverflow thread:
+#
+# http://stackoverflow.com/questions/456672/class-factory-in-python
+#
+# Thank you Alec Thomas!
+
 
 from django.db import models
 from server.models import Server
 from report.models import Variable
-
+from datetime import date
 import logging
 
 
 logger = logging.getLogger(__name__)
 
 
-# TODO ADD a cache table:
-
-# hash (server id + variable id) variable-value
-# stores lasts checked values for all variables.
-# also add to history (or snapshot, whatever) those checks.
-
-# for last status, check cache table.
-
-
 class Snapshot(models.Model):
     """
     """
-    server = models.ForeignKey(Server)
     time = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
+
+    @classmethod
+    def is_snapshot_for(cls, variable):
+        """
+        """
+        raise NotImplementedError
 
     @classmethod
     def take_snapshot(cls, server, must_save=True, **kwargs):
@@ -51,11 +54,18 @@ class VariableSnapshot(Snapshot):
     """
     Stores the variable value of some server in given time.
     """
+    server = models.ForeignKey(Server)
     variable = models.ForeignKey(Variable)
     value = models.IntegerField()
 
     def __unicode__(self):
         return u"%s=%s" % (self.variable.name, self.value)
+
+    @classmethod
+    def is_snapshot_for(cls, variable):
+        """
+        """
+        return variable.data_type != 'c'
 
     @classmethod
     def take_snapshot(cls, server, must_save=True, **kwargs):
@@ -64,18 +74,11 @@ class VariableSnapshot(Snapshot):
         this moment.
         """
         variable = kwargs['variable']
-        result = None
-        if variable.data_type <> 'c': 
-            value = server.show_status(pattern=variable.name)
-            if len(value.keys()) == 0:  # pattern not found
-                return None
-            result = cls(server=server, variable=variable,
-                    value=value[variable.name])
-        else:
-            result = []
-            for d in server.do_task(variable.name):
-                # u = Usage()
-                pass
+        value = server.show_status(pattern=variable.name)
+        if len(value.keys()) == 0:  # pattern not found
+            return None
+        result = cls(server=server, variable=variable,
+                value=value[variable.name])
         if must_save and result:
             result.save()
         return result
@@ -100,6 +103,7 @@ class VariableSnapshot(Snapshot):
             s = cls.take_snapshot(server, v, False)
             if s:
                 values.append(s)
+        return values
 
 
 class UsageSnapshot(Snapshot):
@@ -107,13 +111,50 @@ class UsageSnapshot(Snapshot):
     Represents databases usage in a server. It is a representative usage,
     because it not shows completly active use per database.
     """
-    database = models.CharField(_("Database"), help_text="")
+    database = models.ForeignKey(Database)
     qid = models.PositiveIntegerField(_("Value"), help_text="Current value"\
             " for this date.")
     duration = models.PositiveIntegerField(_("Query time"), help_text="")
+
+    def __unicode__(self):
+        return u"Database='%s' qid=%s d=%s time='%s'" % (self.database,
+                self.qid, self.duration, self.time)
+
+    @classmethod
+    def take_snaptshot(cls, server, must_save=True, **kwargs):
+        """
+        """
+        result = []
+        for u in server.show_processlist():
+            qid, duration = int(u['id']), int(u['time'])
+            try:
+                db = Database.objects.get(server__id=server.id, name=u['db'])
+            except Database.DoesNotExist:
+                db = Database(server=server, name=d['db'])
+                if must_save:
+                    db.save()
+            try:
+                s = cls.objects.get(database__id=db.id, qid=qid,
+                        duration__gte=duration, time__gte=date.today())
+            except cls.DoesNotExist:
+                s = cls(server=server, database=db, qid=qid, duration=duration)
+                if must_save:
+                    s.save()
+            result.append(s)
+        return result
+
+    @classmethod
+    def is_snapshot_for(cls, variable):
+        """
+        """
+        return variable.data_type == 'c' and variable.name == 'USAGE'
 
 
 class SnapshotFactory(object):
     """
     """
-    pass    
+    @take_snapshot(cls, server, must_save=True, **kwargs):
+        for c in Snapshot.__subclasses__():
+            if c.is_snapshot_for(kwargs['variable']):
+                return c.take_snapshot(server, must_save, **kwargs)
+        return None
