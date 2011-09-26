dc723be406c18c12f12c89b592cde3dc943712b3
Ariel Gerardo Rios <arielgerardorios@gmail.com>
2011-09-26 16:33:53 -0300

    ADD: port and host parameters in the example.

--- collector_exec_example.sh
+++ collector_exec_example.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python manage.py collector --stdout=/tmp/collector-out.log --stderr=/tmp/collector-err.log --pidfile=/tmp/collector.pid
+python manage.py collector --stdout=/tmp/collector-out.log --stderr=/tmp/collector-err.log --pidfile=/tmp/collector.pid --host 0.0.0.0 --port 8000
