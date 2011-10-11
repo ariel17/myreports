12e4900342ad45b9ffa576701b0c5d6f1e20c0a9
Ariel Gerardo Rios <arielgerardorios@gmail.com>
2011-10-11 17:12:37 -0300

    ADD: PID file handler. This closes #16

--- collector/management/commands/collector_query.py
+++ collector/management/commands/collector_query.py
@@ -15,9 +15,13 @@ from optparse import make_option
 from server.models import Server
 from collector.models import ServerRPCClientWorker
 from jsonrpclib import Server as JSONRPCClient
+from lockfile import FileLock, LockTimeout
 from sys import exit
 import logging
+from os.path import join
 
+
+SUCCESS, ALREADY_RUNNING_ERROR, CONTEXT_ERROR = range(3)
 
 logger = logging.getLogger(__name__)
 
@@ -25,6 +29,11 @@ logger = logging.getLogger(__name__)
 class Command(BaseCommand):
 
     option_list = BaseCommand.option_list + (
+        make_option('--pidfile', action='store', dest='pidfile',
+            default=join(settings.PROJECT_ROOT, 'collector_query.pid'),
+            help='PID file for this command, to avoid multiple executions. '\
+                    'Default: %s' % join(settings.PROJECT_ROOT,
+                        'collector_query.pid')),
         make_option('--host', action='store', dest='host',
             default=settings.COLLECTOR_CONF['host'],
             help='Host to run query server on. Default: %s' %
@@ -37,10 +46,33 @@ class Command(BaseCommand):
 
     help = ""
     workers = []
+    pidfile = None
 
     def handle(self, *args, **options):
         """
         """
+<<<<<<< HEAD
+        logger.info(">>> Collector RPC client started.")
+
+        #Make pid lock file
+        logger.debug("Locking PID file %s" % options['pidfile'])
+        self.pidfile = FileLock(options['pidfile'])
+        if self.pidfile.is_locked():
+            logger.error("PID file is already locked (other process "\
+                "running?).")
+            exit(ALREADY_RUNNING_ERROR)
+        try:
+            self.pidfile.acquire(
+                    timeout=settings.COLLECTOR_CONF['pidlock_timeout'])
+        except LockTimeout:
+            self.context.pidfile.release()
+            logger.exception("Can't lock PID file:")
+            logger.info(">>> Collector RPC client finished with errors.")
+            exit(CONTEXT_ERROR)
+        print self.pidfile.path    
+=======
+>>>>>>> parent of a56c501... CHG: removed ServerWorker class.
+
         rpc_url = "http://%s:%d" % (options['host'], options['port'])
         logger.debug("Contacting to RPC server: %s" % rpc_url)
         c = JSONRPCClient(rpc_url)
@@ -50,3 +82,6 @@ class Command(BaseCommand):
 
         [w.start() for w in self.workers]
         [w.join() for w in self.workers]
+
+        self.pidfile.release()
+        logger.info(">>> Collector RPC client finished successful.")
