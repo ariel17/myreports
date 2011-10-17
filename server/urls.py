5c8fe81f3aad3b53d7ba32e6ccfb0a921d21b3e9
Ariel Gerardo Rios <arielgerardorios@gmail.com>
2011-10-17 18:37:54 -0300

    ADD: routing /server/test/ view to server view.

--- server/urls.py
+++ server/urls.py
@@ -8,9 +8,10 @@ __author__ = "Ariel Gerardo Ríos (ariel.gerardo.rios@gmail.com)"
 
 
 from django.conf.urls.defaults import patterns, url
-from server.views import show_all_reports
+from server.views import show_all_reports, test_connection
 
 
 urlpatterns = patterns('',
     url(r'^(?P<id>\d+)/$', show_all_reports, name='show_all_reports'),
+    url(r'^test/$', test_connection, name='test_connection'),
 )
