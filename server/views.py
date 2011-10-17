a86fa31294cbf0ff5363c215ac1581366c1ab623
Ariel Gerardo Rios <arielgerardorios@gmail.com>
2011-10-17 18:36:42 -0300

    ADD: 'test_connection' view in 'server' application with JSON response.

--- server/views.py
+++ server/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from server.models import Server, ReportByServer
 from history.models import VariableSnapshot
+import simplejson
 import logging
 
 
@@ -43,3 +44,27 @@ def show_all_reports(request, ip=None, id=None):
     logger.debug("Params: %s" % repr(params))
 
     return render_to_response('reports.html', params)
+
+
+def test_connection(request):
+    """
+    Tests a MySQL connection. Returns a boolean result in JSON format.
+    """
+    params = {"ip": False, "user": False, "password": False, }
+    for p in params:
+        if p in request.REQUEST:
+            params[p] = request.REQUEST[p]
+        else:
+            logger.warning("Missing parameter for request to test "\
+                    "connection: '%s'" % p)
+            json = simplejson.dumps({"result": False, })
+            return HttpResponse(json, mimetype='application/json')
+                
+
+    logger.info("Request to test connection with parameters: %s" % repr(params))
+
+    s = Server(**params)
+    json = simplejson.dumps({"result": self.test_connection(), })
+
+    logger.info("Response: %s" % json)
+    return HttpResponse(json, mimetype='application/json')
