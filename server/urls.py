40f8b9cee667d837c627afab451987bab2784946
Ariel Gerardo Rios <arielgerardorios@gmail.com>
2011-12-02 22:17:43 -0300

    CHG: new pattern for urls

--- server/urls.py
+++ server/urls.py
@@ -8,16 +8,19 @@ __author__ = "Ariel Gerardo Ríos (ariel.gerardo.rios@gmail.com)"
 
 
 from django.conf.urls.defaults import patterns, url
-from server.views import show_all_reports, test_connection
+from server.views import show_all_servers, show_server_id, show_server_ip, \
+        show_server_name, test_connection
 
 
 urlpatterns = patterns('',
 
-    url(r'^(?P<id>\d+)/$', show_all_reports, name='show_reports'),
+    url(r'^$', show_all_servers, name='all_servers'),
 
-    url(r'^(?P<ip>[0-9\.]{7,15})/$', show_all_reports, name='show_reports'),
+    url(r'^(?P<id>\d+)/$', show_server_id, name='server_id'),
 
-    url(r'^(?P<name>[\w\-]+)/$', show_all_reports, name='show_reports'),
+    url(r'^(?P<ip>[0-9\.]{7,15})/$', show_server_ip, name='server_ip'),
+
+    url(r'^(?P<name>[\w\-]+)/$', show_server_name, name='server_name'),
 
     url(r'^test/$', test_connection, name='test_connection'),
 )
