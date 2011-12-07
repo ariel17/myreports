fc9610c2f1a244f785e40a53bd4ebea9c1eada25
Ariel Gerardo Rios <arielgerardorios@gmail.com>
2011-12-07 20:22:47 -0300

    FIX: adjusting the views to make them more generic.

--- server/views.py
+++ server/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
 from server.models import Server, ServerFactory, ReportByServer
 import simplejson
 import logging
@@ -11,7 +12,8 @@ logger = logging.getLogger(__name__)
 def show_all_servers(request):
     """
     """
-    return show_reports(request, Server.objects.filter(active=True))
+    return show_reports(request, Server.objects.filter(active=True),
+            with_general_index=True)
 
 
 def show_server_id(request, id):
@@ -32,27 +34,43 @@ def show_server_name(request, name):
     return show_reports(request, get_object_or_404(Server, name=name))
 
 
-def show_reports(request, servers):
-    info = []
+def show_reports(request, servers, with_general_index=False):
+    ss = []
     for s in servers:
         p = {
             'server': s,
             'permalink': s.get_absolute_url(),
-            'reports': {},
-            'sections': {},
+            'reports': [],
             }
         for r in s.reports.all():
             rbs = ReportByServer.objects.get(server=s, report=r)
-            p['reports'][r.id] = r.get_absolute_url()
+            rd = {
+                    'permalink': r.get_absolute_url(),
+                    'rbs': rbs,
+                    'report': r,
+                    'sections': [],
+                    }
             for se in r.sections.all():
-                variables = se.variables.all()
-                p['sections'][s.id] = {
-                        'permalink': s.get_absolute_url(),}
-                        # s.get_absolute_url(rbs.id),}
-        info.append(p)                
-    logger.debug("Params: %s" % repr(p))
+                sd = {
+                        'permalink': se.get_absolute_url(),
+                        'section': se,
+                        'variables': [],
+                        }
+                for v in se.variables.all():
+                    sd['variables'].append({
+                            'minutes': "s%dv%d.rrd-minutes.png" % (s.id, v.id),
+                            'daily': "s%dv%d.rrd-daily.png" % (s.id, v.id),
+                            'weekly': "s%dv%d.rrd-weekly.png" % (s.id, v.id),
+                            'monthly': "s%dv%d.rrd-monthly.png" % (s.id, v.id),
+                            'yearly': "s%dv%d.rrd-yearly.png" % (s.id, v.id),})
+                rd['sections'].append(sd)
+            p['reports'].append(rd)
+        ss.append(p)                
 
-    return render_to_response('reports.html', p)
+    logger.debug("Rendering: %s" % repr(ss))
+    return render_to_response('reports.html', \
+            {'servers': ss, 'with_general_index': with_general_index,},
+            context_instance=RequestContext(request) )
 
 
 def test_connection(request):
