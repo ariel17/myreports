19ce4f0c8033f627449ca184235bba265c8be3d2
Ariel Gerardo Rios <arielgerardorios@gmail.com>
2011-12-07 20:21:55 -0300

    CHG: Just removed all this code because it is not useful anymore.

--- report/views.py
+++ report/views.py
@@ -7,65 +7,11 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def show_report(request, rbs_id):
-    """
-    """
-    rs = get_object_or_404(ReportByServer, id=id)
-    params = {
-            'name': 'report',
-            'title': rs.report.title,
-            'subtemplate': 'report.html',
-            'server': rs.server,
-            'report': rs.report,
-            'reports': {rs.report.id: rs.report.get_absolute_url(id),},
-            'sections': {},
-    }
-
-    for s in rs.report.sections.all():
-        ss = None
-        if s.current:
-            if rs.server.connect():
-                pass
-            else:
-                # TODO: return 500
-                pass
-        else:
-            logger.debug("History collected for section: %s" % repr(ss))
-        params['sections'][s.id] = {
-                'permalink': s.get_absolute_url(id, s.id)
-        }
-
-    logger.debug("Params to use: %s" % repr(params))
-    return render_to_response('direct.html', params)
+def show_report(request, id=None, name=None):
+    """TODO: add some docstring for show_report"""
+    pass
 
 
-def show_section(request, rbs_id, s_id):
-    """
-    """
-    s = get_object_or_404(Section, id=s_id)
-    rs = get_object_or_404(ReportByServer, id=rbs_id)
-    params = {
-            'name': 'section',
-            'title': s.title,
-            'subtemplate': 'section.html',
-            'server': rs.server,
-            'section': s,
-            'sections': {},
-    }
-
-    ss = None
-    if s.current:
-        if rs.server.connect():
-            pass
-        else:
-            # TODO: return 500
-            pass
-    else:
-        pass
-
-    params['sections'][s.id] = {
-            'permalink': s.get_absolute_url(reportbyserver_id, s.id)
-    }
-
-    logger.debug("Params to use: %s" % repr(params))
-    return render_to_response('direct.html', params)
+def show_section(request, id=None, name=None, report_id=None):
+    """TODO: add some docstring for show_section"""
+    pass
