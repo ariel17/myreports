5af3f9af506a868c8ff9f63b888634a1321bf18c
Ariel Gerardo Rios <arielgerardorios@gmail.com>
2012-01-05 09:58:17 -0300

    CHG: replaced tests for 'collector' application.

--- collector/tests.py
+++ collector/tests.py
@@ -9,44 +9,25 @@ __author__ = "Ariel Gerardo Ríos (arielgerardorios@gmail.com)"
 
 from django.test import TestCase
 from server.models import Server
-from models import QueryWorker
-from jsonrpclib import Server as JSONRPCClient
-import logging
+from models import RPCHandler
 
 
-logger = logging.getLogger(__name__)
-
-
-RPC_HOST = "127.0.0.1"
-RPC_PORT = 9009
-
-
-class TestRPC(TestCase):
+class TestRPCHandler(TestCase):
     """
     """
 
     def setUp(self):
-        logger.info("Setting up RPC Server at http://%s:%d" % (RPC_HOST,
-            RPC_PORT))
-        self.servers = [s for s in Server.objects.all() if s.connect()]
-        if not self.servers:
-            raise Exception("There is none MySQL server to connect and "\
-                    "perform tests.")
-        self.qw = QueryWorker(id=1, servers=self.servers, host=RPC_HOST,
-                port=RPC_PORT)
-        self.qw.start()
-
-    def test_message_ok(self):
-        """
-        """
-        pattern = 'Uptime'
-        c = JSONRPCClient("http://%s:%d" % (RPC_HOST, RPC_PORT))
-        result = c.call_method(1, 'show_status', {'pattern': pattern})
-
-        d = dict(result)
-        self.assertTrue(pattern in d)
+        self.s = Server(ip="127.0.0.1", username="root", name="localhost_test")
+        self.s.save()
+        self.s.connect()
 
     def tearDown(self):
-        self.qw.stop()
-        self.qw.join()
-        [s.close() for s in self.servers]
+        self.s.close()
+
+    def test_call_method(self):
+        """
+        Test remote call to a Server method.
+        """
+        self.rh = RPCHandler([self.s,])
+        r = self.rh.call_method(self.s.id, "show_processlist")
+        self.assertTrue(len(r))
