a973cfc8f3a54f4d99a670d5dccbd66b6939c597
Ariel Gerardo Rios <arielgerardorios@gmail.com>
2012-01-06 21:57:36 -0300

    ADD: a file header. CHG: removed example test.

--- report/tests.py
+++ report/tests.py
@@ -1,16 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
+Description: Test suite for Django application 'report'.
 """
+__author__ = "Ariel Gerardo Ríos (ariel.gerardo.rios@gmail.com)"
 
-from django.test import TestCase
 
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
